a97760eaa2314144f3651a76dfb2c6373ce3f77c
truemanburbank <juxtap.se@gmail.com>
2023-09-13 17:07:00 +0900

    프로젝트 추가

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,67 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+// 1. ¿ßƒ° ∫§≈Õ
+// 2. πÊ«‚ ∫§≈Õ
+
+struct MyVector
+{
+    public float x;
+    public float y;
+    public float z;
+
+
+    public float magnitude { get { return Mathf.Sqrt(x*x + y*y + z*z); } }
+    public MyVector normalized { get { return new MyVector(x/magnitude, y/magnitude, z/magnitude); } }
+
+    public MyVector(float x, float y, float z) { this.x = x; this.y = y; this.z = z; }
+
+    public static MyVector operator +(MyVector a, MyVector b)
+    {
+        return new MyVector(a.x + b.x, a.y + b.y, a.z + b.z);
+    }
+    public static MyVector operator -(MyVector a, MyVector b)
+    {
+        return new MyVector(a.x - b.x, a.y - b.y, a.z - b.z);
+    }
+
+    public static MyVector operator *(MyVector a, float d)
+    {
+        return new MyVector(a.x * d, a.y * d, a.z + d);
+    }
+}
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField]
+    float _speed = 10.0f;
+    void Start()
+    {
+        MyVector to = new MyVector(10.0f, 0.0f, 0.0f);
+        MyVector from = new MyVector(5.0f, 0.0f, 0.0f);
+        MyVector dir = to - from; // (5.0f, 0.0f, 0.0f)
+
+        dir = dir.normalized; // (1.0f, 0.0f, 0.0f)
+
+        MyVector newPos = from + dir * _speed;
+
+        dir = dir.normalized;
+    }
+
+    // GameObject (Player)
+    // Transform
+    // PlayerController
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.W))
+            transform.Translate(Vector3.forward * Time.deltaTime * _speed);
+        if (Input.GetKey(KeyCode.S))
+            transform.Translate(Vector3.back * Time.deltaTime * _speed);
+        if (Input.GetKey(KeyCode.A))
+            transform.Translate(Vector3.left * Time.deltaTime * _speed);
+        if (Input.GetKey(KeyCode.D))
+            transform.Translate(Vector3.right * Time.deltaTime * _speed);
+    }
+}
